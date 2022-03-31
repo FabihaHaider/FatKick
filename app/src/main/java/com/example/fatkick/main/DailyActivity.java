906ab62c3ff39bd343bdbe5bb21ba1938ca8ae5a
tuba <tasnuvatuba99@gmail.com>
2022-03-31 08:42:33 +0600

    Initial commit

---
+++ app/src/main/java/com/example/fatkick/main/DailyActivity.java
@@ -0,0 +1,11 @@
+package com.example.fatkick.main;
+
+public class DailyActivity {
+    Double calorieIntake; //in Kcal
+    Double waterIntake;   //in L
+    Double sleep;         //in hour
+    Double meditation;    //in hour
+    Exercise exercise[];
+
+
+}
