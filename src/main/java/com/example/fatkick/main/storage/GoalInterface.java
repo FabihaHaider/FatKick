36ea22ae3920862c0250b43ac4d47d8d084f165c
fabiha <fabihahaider4@gmail.com>
2022-04-07 22:10:25 +0600

    "fabiha's commit"

--- src/main/java/com/example/fatkick/main/storage/GoalInterface.java
+++
@@ -1,8 +0,0 @@
-package com.example.fatkick.main.storage;
-
-import com.example.fatkick.main.authenticator.User;
-import com.example.fatkick.main.goal_setting.FinalGoal;
-
-public interface GoalInterface {
-    void onCallBack(FinalGoal finalGoal);
-}
--- src/main/java/com/example/fatkick/main/storage/UserInterface.java
+++
@@ -1,7 +0,0 @@
-package com.example.fatkick.main.storage;
-
-import com.example.fatkick.main.authenticator.User;
-
-public interface UserInterface {
-    void onCallBack(User user);
-}
